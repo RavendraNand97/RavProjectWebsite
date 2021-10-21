90e476346ef4c00021d4a52388d02d375fc99a7f
RavendraNand97 <ravendranand97@gmail.com>
2021-10-21 15:10:03 -0400

    server trial

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
 
-
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
